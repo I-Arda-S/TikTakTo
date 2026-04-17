922ab67d60d006d3ec951000b6376753b8ab1cc4
I-Arda-S <ardasarihan48@gmail.com>
2026-04-17 22:26:55 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Satranc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tictac = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BoyutSec = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tictac
+            // 
+            this.tictac.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tictac.ColumnCount = 8;
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.Location = new System.Drawing.Point(229, 12);
+            this.tictac.Name = "tictac";
+            this.tictac.RowCount = 8;
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tictac.Size = new System.Drawing.Size(400, 400);
+            this.tictac.TabIndex = 0;
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.Location = new System.Drawing.Point(13, 40);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(121, 56);
+            this.btnSifirla.TabIndex = 1;
+            this.btnSifirla.Text = "Sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BoyutSec);
+            this.panel1.Controls.Add(this.btnSifirla);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 180);
+            this.panel1.TabIndex = 2;
+            // 
+            // BoyutSec
+            // 
+            this.BoyutSec.FormattingEnabled = true;
+            this.BoyutSec.Items.AddRange(new object[] {
+            "3x3",
+            "4x4",
+            "5x5",
+            "6x6",
+            "7x7",
+            "8x8",
+            "9x9"});
+            this.BoyutSec.Location = new System.Drawing.Point(13, 10);
+            this.BoyutSec.Name = "BoyutSec";
+            this.BoyutSec.Size = new System.Drawing.Size(121, 24);
+            this.BoyutSec.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tictac);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tictac;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox BoyutSec;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Satranc
+{
+    public partial class Form1:Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BoyutSec.SelectedIndex = 0;
+            OyunTahtasiniOlustur(tictac, 3, 3);
+        }
+
+        private bool turXde = false;
+        private string[,] matrisTahta;
+        private int N=3;
+
+        private void OyunTahtasiniOlustur(TableLayoutPanel tahta, int en, int boy)
+        {
+            tahta.Controls.Clear();
+            tahta.RowStyles.Clear();
+            tahta.ColumnStyles.Clear();
+            matrisTahta = new string[en, boy];
+
+            tahta.RowCount = 0;
+            tahta.ColumnCount = 0;
+            for(int i = 0;i < boy;i++)
+                tahta.RowStyles.Add(new RowStyle(SizeType.Percent,100f / boy));
+            for(int j = 0;j < en;j++)
+                tahta.ColumnStyles.Add(new ColumnStyle(SizeType.Percent,100f / en));
+            tahta.RowCount = boy;
+            tahta.ColumnCount = en;
+
+            float fontBoyutu = tahta.Height / tahta.ColumnCount * 0.4f;
+            for(int i = 0; i<tahta.ColumnCount;i++)
+            {
+                for(int j = 0; j<tahta.RowCount;j++)
+                {
+                    Button kare = new Button
+                    {
+                        Dock = DockStyle.Fill,
+                        Tag = new Point(i,j),
+                        Margin = new Padding(0),
+                        Font = new Font("Arial",fontBoyutu,FontStyle.Regular),
+                        BackColor = Color.Brown
+                    };
+                    //if((j%2==0 && i%2==0) || (j%2!=0 && i%2!=0))
+                    //kare.Text = "X";
+                    //kare.BackColor = Color.SandyBrown;
+
+                    kare.Click += Kare_click;
+                    tahta.Controls.Add(kare,j,i);
+                }
+            }
+        }
+
+        private void btnSifirla_Click(object sender,EventArgs e)
+        {
+            N = BoyutSec.SelectedIndex+3;
+            turXde = false;
+            OyunTahtasiniOlustur(tictac,N,N);
+        }
+
+        private void Kare_click(object sender, EventArgs e)
+        {
+            Button kare = sender as Button;
+            var konum = tictac.GetPositionFromControl(kare);
+            int x = konum.Row; 
+            int y = konum.Column;
+
+            if(turXde)
+            {
+                kare.Text = "X";
+                matrisTahta[x,y] = "X";
+                turXde = false;
+            }
+            else
+            {
+                kare.Text = "O";
+                matrisTahta[x,y] = "O";
+                turXde = true;
+            }
+
+
+            if(KontrolEt(x,y,kare.Text))
+                MessageBox.Show("Kazandın.");
+            kare.Enabled = false;
+        }
+
+        private bool KontrolEt(int satir,int sutun,string isaret)
+        {
+            int kazanmakİcinGerekenKareSayisi = N; // Mesela 3x3'te ardışık 3 tane aynı olunca kazanılır ya onu kast ettim. Çok kare olduuğnda da 5'i koşul sağlamak standart imiş.
+            if(N>5)
+                kazanmakİcinGerekenKareSayisi=5;
+
+            if(YatayKontrol(satir,isaret,kazanmakİcinGerekenKareSayisi)) 
+                return true;
+            if(DikeyKontrol(sutun,isaret,kazanmakİcinGerekenKareSayisi)) 
+                return true;
+
+            // 3. Çapraz Kontrol (Eğer tıklanan yer çapraz hat üzerindeyse)
+            //if(CaprazBak(isaret)) return true;
+
+            return false;
+        }
+        /*private string GetCellText(int r,int c)
+        {
+            var ctrl = tableLayoutPanel1.GetControlFromPosition(c,r);
+            return ctrl?.Text ?? "";
+        }*/
+        private bool DikeyKontrol(int sutun,string isaret,int gerekenSira)
+        {
+            int sayac = 0;
+            for(int i = 0;i < N;i++)
+            {
+                if(matrisTahta[i,sutun] == isaret)
+                {
+                    sayac++;
+                    if(sayac == gerekenSira) return true;
+                }
+                else
+                    sayac = 0;
+
+            }
+            return false;
+        }
+        private bool YatayKontrol(int satir,string isaret, int gerekenSira)
+        {
+            int sayac = 0;
+            for(int j=0;j<N;j++)
+            {
+                if(matrisTahta[satir,j] == isaret)
+                {
+                    sayac++;
+                    if(sayac==gerekenSira) return true;
+                }
+                else
+                    sayac = 0;
+            }
+            return false;
+        }
+        /*private bool YatayBak(int r,string isaret)
+        {
+            for(int c = 0;c < tableLayoutPanel1.ColumnCount;c++)
+            {
+                if(GetCellText(r,c) != isaret) return false;
+            }
+            return true;
+        }
+        private bool CaprazBak(string isaret)
+        {
+            bool solUst = true;
+            for(int i = 0;i < tableLayoutPanel1.RowCount;i++)
+            {
+                if(GetCellText(i,i) != isaret) solUst = false;
+            }
+            // Sağ üstten sol alta olan diğer çaprazı da benzer şekilde kontrol etmelisin
+            return solUst;
+        }*/
+
+    }
+}
